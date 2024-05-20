422f068216eae0c23d7388371e102dbc10ddd881
Jarin Mahmud <jarinmahmud@gmail.com>
2024-05-19 21:37:12 -0400

    Prev Code

---
+++ src/main/java/com/ps/Dealership.java
@@ -0,0 +1,111 @@
+package com.ps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    //Constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getPrice() >= min && vehicle.getPrice() <= max) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getMake().equalsIgnoreCase(make) && vehicle.getModel().equalsIgnoreCase(model)) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+
+    public List<Vehicle> getAllVehicles() {
+        return new ArrayList<>(inventory);
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getYear() >= min && vehicle.getYear() <= max) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesByColor(String color) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getColor().equalsIgnoreCase(color)) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getOdometer() >= min && vehicle.getOdometer() <= max) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        List<Vehicle> result = new ArrayList<>();
+        for (Vehicle vehicle : inventory) {
+            if (vehicle.getVehicleType().equalsIgnoreCase(vehicleType)) {
+                result.add(vehicle);
+            }
+        }
+        return result;
+    }
+
+    //adding vehicle
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    //removing vehicle
+    public void removeVehicle(Vehicle vehicle) {
+        inventory.remove(vehicle);
+    }
+
+}
+
---
+++ src/main/java/com/ps/DealershipFileManager.java
@@ -0,0 +1,60 @@
+package com.ps;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class DealershipFileManager {
+    private static final String FILE_PATH = "workData.txt";
+
+    //reading vehicle information
+    public static Dealership getDealership() {
+        Dealership dealership = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
+            String dealershipInfo = reader.readLine();
+            String[] dealershipData = dealershipInfo.split("\\|");
+            String name = dealershipData[0];
+            String address = dealershipData[1];
+            String phone = dealershipData[2];
+            dealership = new Dealership(name, address, phone);
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] vehicleData = line.split("\\|");
+                int vin = Integer.parseInt(vehicleData[0]);
+                int year = Integer.parseInt(vehicleData[1]);
+                String make = vehicleData[2];
+                String model = vehicleData[3];
+                String vehicleType = vehicleData[4];
+                String color = vehicleData[5];
+                int odometer = Integer.parseInt(vehicleData[6]);
+                double price = Double.parseDouble(vehicleData[7]);
+                Vehicle vehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+                dealership.addVehicle(vehicle);
+            }
+        } catch (Exception e) {
+
+        }
+        return dealership;
+    }
+
+    //storing vehicle information
+    public static void saveDealership(Dealership dealership) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            // Writing dealership information
+            writer.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
+            writer.newLine();
+
+            // Writing vehicle information
+            for (Vehicle vehicle : dealership.getAllVehicles()) {
+                writer.write(vehicle.getVin() + "|" + vehicle.getYear() + "|" + vehicle.getMake() + "|" +
+                        vehicle.getModel() + "|" + vehicle.getVehicleType() + "|" + vehicle.getColor() + "|" +
+                        vehicle.getOdometer() + "|" + vehicle.getPrice());
+                writer.newLine();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
